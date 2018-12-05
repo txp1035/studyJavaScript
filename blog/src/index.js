7f79ebc0a0e9fb26774caa53e3ae45c1aaab7a63
DaoYuan <txp1035@gmail.com>
2018-12-05 18:58:30 +0800

    页面模板

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,21 +1,21 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const flash = require('connect-flash');
-const config = require('config-lite')(__dirname);
-const routes = require('./routes');
-const pkg = require('../package');
+const path = require('path')
+const express = require('express')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const flash = require('connect-flash')
+const config = require('config-lite')(__dirname)
+const routes = require('./routes')
+const pkg = require('../package')
 
-const app = express();
+const app = express()
 
 // 设置模板目录
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'))
 // 设置模板引擎为 ejs
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 // 设置静态文件目录
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 // session 中间件
 app.use(
   session({
@@ -31,14 +31,28 @@ app.use(
       url: config.mongodb // mongodb 地址
     })
   })
-);
+)
 // flash 中间件，用来显示通知
-app.use(flash());
+app.use(flash())
+
+// 设置模板全局常量
+app.locals.blog = {
+  title: pkg.name,
+  description: pkg.description
+}
+
+// 添加模板必需的三个变量
+app.use(function (req, res, next) {
+  res.locals.user = req.session.user
+  res.locals.success = req.flash('success').toString()
+  res.locals.error = req.flash('error').toString()
+  next()
+})
 
 // 路由
-routes(app);
+routes(app)
 
 // 监听端口，启动程序
-app.listen(config.port, function() {
-  console.log(`${pkg.name} listening on port ${config.port}`);
-});
+app.listen(config.port, function () {
+  console.log(`${pkg.name}\nhttp://127.0.0.1:${config.port}`)
+})
