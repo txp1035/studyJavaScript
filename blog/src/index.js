3ed6a61511be1944819923e815af276119b7f12a
DaoYuan <txp1035@gmail.com>
2018-12-05 12:45:39 +0800

    博客模块功能差数据库连接

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,20 +1,44 @@
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('connect-flash');
+const config = require('config-lite')(__dirname);
+const routes = require('./routes');
+const pkg = require('../package');
+
 const app = express();
 
-app.use(function(req, res, next) {
-  console.log('1');
-  next(new Error('haha'));
-});
+// 设置模板目录
+app.set('views', path.join(__dirname, 'views'));
+// 设置模板引擎为 ejs
+app.set('view engine', 'ejs');
 
-app.use(function(req, res, next) {
-  console.log('2');
-  res.status(200).end();
-});
+// 设置静态文件目录
+app.use(express.static(path.join(__dirname, 'public')));
+// session 中间件
+app.use(
+  session({
+    name: config.session.key, // 设置 cookie 中保存 session id 的字段名称
+    secret: config.session.secret, // 通过设置 secret 来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改
+    resave: true, // 强制更新 session
+    saveUninitialized: false, // 设置为 false，强制创建一个 session，即使用户未登录
+    cookie: {
+      maxAge: config.session.maxAge // 过期时间，过期后 cookie 中的 session id 自动删除
+    },
+    store: new MongoStore({
+      // 将 session 存储到 mongodb
+      url: config.mongodb // mongodb 地址
+    })
+  })
+);
+// flash 中间件，用来显示通知
+app.use(flash());
 
-//错误处理
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
+// 路由
+routes(app);
 
-app.listen(3000);
+// 监听端口，启动程序
+app.listen(config.port, function() {
+  console.log(`${pkg.name} listening on port ${config.port}`);
+});
