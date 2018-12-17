7769a62c770356e1f613493037d1f6c20e23accb
DaoYuan <txp1035@gmail.com>
2018-12-17 12:43:21 +0800

    整理

--- blog/src/index.js
+++ blog/src/index.js
@@ -102,6 +102,6 @@ if (module.parent) {
 } else {
   // 监听端口，启动程序
   app.listen(config.port, function () {
-    console.log(`${pkg.name} listening on port ${config.port}`)
+    console.log(`${pkg.name} listening on http://127.0.0.1:${config.port}`)
   })
 }
